78c254b3c633856adec7ed5b05c7a501d5a08181
mc429 <machaoem@163.com>
2018-12-22 15:20:44 +0800

    Initial Commit

---
+++ MACCircle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MACCircle
+//
+//  Created by Marco on 2018/12/22.
+//  Copyright © 2018年 Marco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MACCircle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MACCircle
+//
+//  Created by Marco on 2018/12/22.
+//  Copyright © 2018年 Marco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
